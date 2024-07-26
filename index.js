54afbc85c31f429ceffe919f862aa39b4107dd49
PittnoiR <omargarza394@gmail.com>
2024-07-26 00:17:44 -0600

    ip/traker v.Movile

---
+++ index.js
@@ -0,0 +1,55 @@
+const $ = document;
+const input = $.querySelector("input")
+const btton= $.querySelector("button")
+const inf= {
+    ip: $.querySelector("#ip"),
+    locationn: $.querySelector("#location"),
+    timezone:$.querySelector("#timezone"),
+    isp:$.querySelector("#isp"),
+}
+
+
+var map = L.map('mapid').setView([51.505, -0.09], 13);
+
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+function updateMap(lat, lng,txt) {
+    map.setView([lat, lng], 13);
+    map.eachLayer(function(layer){
+        if(layer instanceof L.Marker){
+            map.removeLayer(layer);
+        }
+    });
+
+    // agregaa un nuevo marcador en la nueva ubicación
+    L.marker([lat, lng]).addTo(map)
+    .bindPopup(txt)
+    .openPopup();
+}
+
+
+btton.addEventListener("click",(e)=>{
+    e.preventDefault()
+    console.log("clicked")
+    fetch(` https://geo.ipify.org/api/v2/country,city?apiKey=at_jbpiz6fhcgjEbT1CEZehHCH99SbcS&ipAddress=${input.value}`)
+    .then(responce=> responce.json())
+        .then(data=>{
+            console.log(data)
+            inf.ip.textContent = data.ip;
+            inf.locationn.textContent = `${data.location.country}-${data.location.region}`;
+            inf.timezone.textContent = `UTC${data.location.timezone}`;
+            inf.isp.textContent = data.isp;
+            updateMap(data.location.lat, data.location.lng,data.location.city);
+            input.value = ""
+            var icon  =  document.querySelector(".leaflet-marker-icon")
+            icon.src = "./location.svg"
+            icon.style.width = "50px"
+            icon.style.height = "50px"
+        });s
+        
+})
+
+
